2f5ff81246a3338e99f4aae715008e54632be914
Kieran <kieran.conlon@gmail.com>
2022-02-23 13:25:37 +0000

    added Geodesic Direct

--- Sources/CGeographicLib/CGeodesic.cpp
+++ Sources/CGeographicLib/CGeodesic.cpp
@@ -18,3 +18,12 @@ void inverse(double lat1, double lon1, double lat2, double lon2,
   geod.Inverse(lat1, lon1, lat2, lon2, *s12, *azi1, *azi2);
 }
 
+void direct(double lat1, double lon1, double azi1, double s12,
+            double *lat2, double *lon2, double * azi2,
+            double a, double f) {
+  using namespace std;
+  using namespace GeographicLib;
+  
+  Geodesic geod(a, f);
+  geod.Direct(lat1, lon1, azi1, s12, *lat2, *lon2, *azi2);
+}
--- Sources/CGeographicLib/include/CGeodesic.h
+++ Sources/CGeographicLib/include/CGeodesic.h
@@ -13,6 +13,10 @@ void inverse(double lat1, double lon1, double lat2, double lon2,
              double *s12, double *azi1, double *azi2,
              double a, double f);
 
+void direct(double lat1, double lon1, double azi1, double s12,
+            double *lat2, double *lon2, double * azi2,
+            double a, double f);
+
 #ifdef __cplusplus
 }
 #endif
