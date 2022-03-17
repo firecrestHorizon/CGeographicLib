31da862b5e42f9744acb3b380ffd0ea154ad9b94
Kieran <kieran.conlon@gmail.com>
2022-03-17 15:41:53 +0000

    Fixed CGeodesic.h

--- Sources/CGeographicLib/include/CGeodesic.hpp
+++ Sources/CGeographicLib/include/CGeodesic.hpp
@@ -20,6 +20,10 @@ void direct(double lat1, double lon1, double azi1, double s12,
             double *lat2, double *lon2, double * azi2,
             double a, double f);
 
+void arcDirect(double lat1, double lon1, double azi1, double a12,
+            double *lat2, double *lon2, double * azi2,
+            double a, double f);
+
 #ifdef __cplusplus
 }
 #endif
